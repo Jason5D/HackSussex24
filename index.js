fa5ff91b99276ebc7ac3e10b078ba6e592034eed
Jason <jason5donoghue@gmail.com>
2024-02-17 15:15:12 +0000

    added initial chatbot function

---
+++ index.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+
+import OpenAI from 'openai'
+const openai = new OpenAI()
+
+const results = await openai.chat.completions.create({
+  model: 'gpt-3.5-turbo',
+  messages: [
+    {
+      role: 'system',
+      content:
+        'You are an AI assistant, answer any questions to the best of your ability.',
+    },
+    {
+      role: 'user',
+      content:
+        'Hi! Can you tell wme what the best way to learn how to learn maths?',
+    },
+  ],
+})
+
+console.log(results.choices[0].message.content)
